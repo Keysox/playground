edc5e7779977a1ccdde3ebdd76de8ac2d3ffb1d2
Alex Kessock <akessock@gmail.com>
2019-02-18 17:16:34 -0700

    Add eslint and enable recommended rules (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ class App extends Component {
         <header>
           <h1>Alex Kessock Photography</h1>
         </header>
-        <img src="https://lh3.googleusercontent.com/dQVzJ31kwETDKHFoJqD2smCxCLY_w5SLT7_-LIgvPChqAjUifh5oot8XU31eEATBPN4rhB3jZd9UWRaTaQJPzXGGO_UM-sRQMA6MC0nGLeEIXLaVmAOrkNVZmbLcaPwT3dw-uhk-Xvs=w1840-h1226-no" />
+        <img
+          alt="Black-throated mango hummingbird"
+          title="Black-throated mango hummingbird"
+          src="https://lh3.googleusercontent.com/Q3yc0m3wbV_RMd_-XqkBmkNK5eGpUZHLAWeKnspKfSYEzafGEbW5GV0AGOIkvTyVZQk0-pnn53l_4H3ptum6D10Eu0qEi_ntvK44Lgav4s82u9BmyfD245eo_oERufxjelTfZxdALJ1rz4x77Gt4kf3TLudodgigRIs3-dfBA5pg4t--tiIURa1m8C35S6FPUCxbxPJSL6VArkUDAAdAL2U6knaBs67r8nn2nAVzqbsNZflvx5LIIZPhkKbqtOY8zWN99-V7AStrKv_iQ932MIa3RJdyZ52kDffjr97BLALks09kAh6TW2mUn8WENPDiWFsEJUEyJxJ2lYSYsdE5MyrcY6faOyKmwQ_0ezaV2D93fRYNclxbc6z0IlZCWShUA2I8M_qintHz4ec4F_p-atf01oQvZkYi6EzrNuKnYD0u_WaBP_ZKK-LUEKohEIi0DvPVi1r3a8xDF56U9irsDDylrp8CSFYmkaL52SyYOqfwrwatBwJz5KQZvhOxXWt7hhiOAkVrvD2z3OaPFWtxbO9cC9McXTH9NnKlSQFk5DbqWoKPU2Sqff8gQ02LuIQu-DJtnMCSgO71O90UR53HC809EJ3HdBu6_YaytRSzECVYQ7U7e0oDVkka6bYygWSdX7JuWSADgVz1m34hpayo5aGmQKVxwUtD=w2912-h1942-no"
+        />
       </>
     )
   }
