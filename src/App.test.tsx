53e2021d294d6bda4054a2eb37026227be4e4301
Alex Kessock <akessock@gmail.com>
2019-05-17 08:31:46 -0600

    Add unit tests to <App /> using React Testing Library (#46)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,47 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import { render } from 'react-testing-library'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
+
+window.history.pushState({}, 'Alex Kessock Photography', '/photography')
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders a header', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('Alex Kessock Photography')).toBeInTheDocument()
+})
+
+it('renders a navbar', () => {
+  const { container } = render(<App />)
+
+  expect(container.querySelectorAll('a').length).toBe(3)
+})
+
+describe('renders images', () => {
+  let allImagesCount: number
+
+  it('on the All page', () => {
+    const { container } = render(<App />)
+    allImagesCount = container.querySelectorAll('img').length
+
+    expect(allImagesCount).toBeGreaterThan(0)
+  })
+
+  it('on the Landscape page', () => {
+    const { container, getByText } = render(<App />)
+
+    getByText('Landscape').click()
+
+    const landscapeImagesCount = container.querySelectorAll('img').length
+    expect(landscapeImagesCount).toBeLessThan(allImagesCount)
+    expect(landscapeImagesCount).toBeGreaterThan(0)
+  })
 })
