32f6b4840d0041a193c980cafe958a124866c04b
Alex Kessock <akessock@gmail.com>
2019-12-23 17:46:59 -0700

    wait longer?

--- cypress/integration/ci/percy.spec.js
+++ cypress/integration/ci/percy.spec.js
@@ -1,7 +1,7 @@
 describe('Integration test with visual testing', function() {
   it('Loads the homepage', function() {
     cy.visit('http://localhost:3000/')
-      .wait(1000)
+      .wait(10000)
       .percySnapshot()
   })
 })
