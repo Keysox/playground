1660f18dfe0a4d3d4bce18b1829b371fd73f610f
Alex Kessock <akessock@gmail.com>
2020-04-20 20:03:29 -0600

    Add link to Instagram (#207)

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,7 +7,15 @@ interface Props {
 function Header(props: Props) {
   return (
     <header>
-      <h1>{props.text}</h1>
+      <h1>
+        <a
+          href="https://www.instagram.com/alexkessock/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {props.text}
+        </a>
+      </h1>
     </header>
   )
 }
